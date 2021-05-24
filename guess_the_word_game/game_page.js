8974bb991bda26156d917cad000922e9b5879ec8
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-05-24 20:16:49 +0530

    Add files via upload
    bye-bye

--- guess_the_word_game/game_page.js
+++ guess_the_word_game/game_page.js
@@ -11,13 +11,13 @@ document.getElementById("Nick-name2").innerHTML = player2 + ":";
 document.getElementById("score_board1").innerHTML = score1;
 document.getElementById("score_board2").innerHTML = score2; 
 
-document.getElementById("question").innerHTML = "question" + player1;
-document.getElementById("answer").innerHTML = "answer" + player2 ;
+document.getElementById("question").innerHTML = "Question Turn-" + player1;
+document.getElementById("answer").innerHTML = "Answer Turn-" + player2 ;
 
  function send()
  {
- word=document.getElementById("word").Value;
- word=word.toLowerCase();
+ get_word=document.getElementById("word").value;
+ word=get_word.toLowerCase();
  console.log(word);
 
 
@@ -34,8 +34,8 @@ char3=word.charAt(length);
 console.log(char3);
 
 replace1=word.replace(char1,"_");
-replace2=word.replace(char2,"_");
-replace3=word.replace(char3,"_");
+replace2=replace1.replace(char2,"_");
+replace3=replace2.replace(char3,"_");
 console.log(replace3);
 
 q1="<h4 id='word_display'>Q." + replace3 +"</h4>";
@@ -44,7 +44,56 @@ button = "<br><br><button onclick='check()' class='btn btn-info'>Check</button>"
 
 row = q1+input_box+ button;
 document.getElementById("output").innerHTML=row;
-document.getElementById("word").innerHTML="";
+document.getElementById("word").value="";
 
 
+
+ }
+
+ question_turn ="player1";
+ answer_turn ="player2"  ;
+
+ function check()
+ {
+    get_answer=document.getElementById("input_check_box").value;
+    answer=get_answer.toLowerCase();
+    
+if(answer==word)
+    {
+    if(answer_turn =="player2"){
+    score2=score2+1;
+    document.getElementById("score_board2").innerHTML=score2;
+    }
+    else{
+        score1=score1+1;
+        document.getElementById("score_board1").innerHTML=score1;
+    }
+
+if(question_turn=="player1")
+{
+    question_turn="player2";
+    document.getElementById("question").innerHTML = "Question Turn-" + player2;
+
+}
+
+else{
+    question_turn="player1";
+    document.getElementById("question").innerHTML = "Question Turn-" + player1;
+
+}
+
+if(answer_turn=="player2")
+{
+    answer_turn="player1";
+    document.getElementById("answer").innerHTML = "Answer Turn- " + player1;
+
+}
+
+else{
+    answer_turn="player2";
+    document.getElementById("answer").innerHTML = "Answer Turn- " + player2;
+
+}
+ }
+ document.getElementById("output").innerHTML=""
  }
